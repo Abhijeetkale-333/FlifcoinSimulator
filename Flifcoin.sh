572bc93ec6d76e451f59254e211883900c420cf8
Your Name <you@example.com>
2021-03-25 15:54:38 +0530

    Added:Number of times head and tail wins

--- Flifcoin.sh
+++ Flifcoin.sh
@@ -1,8 +1,20 @@
-checkRandom=$(($RANDOM%2))
+#! /bin/bash
 
-if [[ $checkRandom -eq 0 ]]
-then
-	echo "TAILS"
-else
-	echo "HEADS"
-fi
+headcount=0
+tailcount=0
+
+
+for (( i=0 ; i<10 ; i++ ))
+do
+	checkRandom=$(($RANDOM%2))
+	if [[ $checkRandom -eq 0 ]]
+	then
+		tailcount=$(($tailcount+1))
+	elif [[ $checkRandom -ne 0 ]]
+	then
+		headcount=$(($headcount+1))
+	fi
+done
+
+echo "Number of Tail wins:" $tailcount
+echo "Number of Head Wins:" $headcount
